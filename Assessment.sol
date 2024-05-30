29c14388d92169b37bb371da4944ea42b825bdb2
ellasordilla <138562279+ellasordilla@users.noreply.github.com>
2024-05-30 20:03:29 +0800

    Update Assessment.sol

--- Assessment.sol
+++ Assessment.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    mapping(address => string) public names;
+    mapping(address => string) public referenceNumbers;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
@@ -13,27 +15,28 @@ contract Assessment {
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
+    }
+
+    function setNameAndReference(string memory _name, string memory _referenceNumber) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        names[msg.sender] = _name;
+        referenceNumbers[msg.sender] = _referenceNumber;
     }
 
     function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -46,13 +49,10 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
     }
 }
